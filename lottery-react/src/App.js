6240eb53400e69e1d117114276d28f380e5252f2
Vincenzo <perronetitan@gmail.com>
2022-04-25 14:55:52 +0200

    Saved session

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -25,14 +25,18 @@ class App extends Component {
     console.log(contractInterface.contractAddress, '\n', contractInterface.abi);
   }
 
-  async componentDidUpdate(prevProp, prevState) {
-    if ((this.state.web3 && prevState.web3 == null)) {
-      console.log('old state', prevState.web3)
+  async componentDidMount()
+  {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      this.setState({web3: new Web3(window.ethereum)});
+
       const accounts = await this.state.web3.eth.getAccounts()
       this.setState({ accountsConnected: accounts });
       const weiBalance = await this.state.web3.eth.getBalance(accounts[0]);
       this.setState({ accountBalance: Web3.utils.fromWei(weiBalance, 'ether') });
       this.setState({ connectionStatus: 'Connected' });
+      this.setState({currentChoice: 'accountDetails'});
       // init contract
       this.setState({
         lottery: new this.state.web3.eth.Contract(contractInterface.abi, contractInterface.contractAddress)
@@ -45,7 +49,6 @@ class App extends Component {
     return (
       <>
         <div style={{margin:'2rem'}}>
-          <ConnectButton onConnect={(web3 => this.setState({ web3,currentChoice: 'accountDetails' }))} />
           {
             (this.state.currentChoice === 'home') ? 
             <Home></Home> :
