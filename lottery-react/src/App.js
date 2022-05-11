b6a58a5c28e999268fe8511e5458c6a3f26baa24
Vincenzo <perronetitan@gmail.com>
2022-05-11 22:06:33 +0200

    Added fix on restore session at startup

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -28,27 +28,28 @@ class App extends Component {
     console.log(contractInterface.contractAddress, '\n', contractInterface.abi);
   }
 
-  async componentDidUpdate()
-  {
-    
-    if (window.ethereum && (this.state.web3 !== null || this.state.web3 !== 'undefined') && this.state.firstRun === true) {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      this.setState({web3: new Web3(window.ethereum)});
-
-      const accounts = await this.state.web3.eth.getAccounts()
-      this.setState({ accountsConnected: accounts });
-      const weiBalance = await this.state.web3.eth.getBalance(accounts[0]);
-      this.setState({ accountBalance: Web3.utils.fromWei(weiBalance, 'ether') });
-      this.setState({ connectionStatus: 'Connected' });
-      this.setState({currentChoice: 'accountDetails'});
-      // init contract
-      this.setState({
-        lottery: new this.state.web3.eth.Contract(contractInterface.abi, contractInterface.contractAddress)
-      });
-      console.log('web', this.state.web3, '\nAccount connected', this.state.accountsConnected[0], '\nAccount balance', this.state.accountBalance);
-      this.setState({firstRun: false});
+  async componentDidUpdate(prevProp, prevState) {
+    if ((this.state.web3 && prevState.web3 == null)) {
+      if (window.ethereum && (this.state.web3 !== null || this.state.web3 !== 'undefined') && this.state.firstRun === true) {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        this.setState({web3: new Web3(window.ethereum)});
+  
+        const accounts = await this.state.web3.eth.getAccounts()
+        this.setState({ accountsConnected: accounts });
+        const weiBalance = await this.state.web3.eth.getBalance(accounts[0]);
+        this.setState({ accountBalance: Web3.utils.fromWei(weiBalance, 'ether') });
+        this.setState({ connectionStatus: 'Connected' });
+        this.setState({currentChoice: 'accountDetails'});
+        // init contract
+        this.setState({
+          lottery: new this.state.web3.eth.Contract(contractInterface.abi, contractInterface.contractAddress)
+        });
+        console.log('web', this.state.web3, '\nAccount connected', this.state.accountsConnected[0], '\nAccount balance', this.state.accountBalance);
+        this.setState({firstRun: false});
+      }
     }
   }
+
 
   async componentDidMount()
   {
